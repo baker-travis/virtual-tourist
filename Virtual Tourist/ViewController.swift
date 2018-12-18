e8547f0769e92148e2c970791f3b84aed2fad1c9
Travis Baker <baker.travis.w@gmail.com>
2018-12-18 16:43:48 -0600

    Initial Commit

---
+++ Virtual Tourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Virtual Tourist
+//
+//  Created by Travis Baker on 12/18/18.
+//  Copyright © 2018 Travis Baker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
